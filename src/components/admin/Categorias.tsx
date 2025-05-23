20b1763b5cc05775491a1eb827efa722db9bc423
DiegoCanales <diegocanales112@gmail.com>
2025-05-23 19:33:01 -0300

    Creacion de Categoria Principal y 1ra fase de SubCategorias

--- src/components/admin/Categorias.tsx
+++ src/components/admin/Categorias.tsx
@@ -1,11 +1,21 @@
 import { ChangeEvent, useState } from 'react'
 import { FaSearch, FaTimes, FaPencilAlt, FaRegTrashAlt, FaAngleUp } from "react-icons/fa";
+import { RubroInsumo } from '../../types/RubroInsumo';
 
 const Categorias = () => {
+  const [modalAbierto, setModalAbierto] = useState<boolean>(false);
+  const [nombreRubro, setNombreRubro] = useState<string>("");
+  const [tipoRubro, setTipoRubro] = useState<string>("");
+  const [rubroPadreSeleccionado, setRubroPadreSeleccionado] = useState<string>("");
+  const [rubros, setRubros] = useState<RubroInsumo[]>([]);
+  const [subCategoriasAbiertas, setSubCategoriasAbiertas] = useState<{ [key: number]: boolean }>({});
 
-  const [busqueda, setBusqueda] = useState<string>("")
-  const [opcionFiltrar, setOpcionFiltrar] = useState<string>('')
-  const [modalAbierto, setModalAbierto] = useState<boolean>(false)
+  const abrirModal = () => {
+    setModalAbierto(!modalAbierto);
+    setNombreRubro("");
+    setTipoRubro("");
+    setRubroPadreSeleccionado("");
+  };
 
   const opcionElegida = (opcion: string) => {
     return setOpcionFiltrar(opcion);
@@ -15,10 +25,59 @@ const Categorias = () => {
     setBusqueda(e.target.value);
   };
 
-  const abrirModal = () => {
-    setModalAbierto(!modalAbierto)
+  const [busqueda, setBusqueda] = useState<string>("")
+  const [opcionFiltrar, setOpcionFiltrar] = useState<string>('')
+
+  const crearRubro = (e: React.FormEvent) => {
+  e.preventDefault();
+  if (!nombreRubro || !tipoRubro) return;
+
+  const nuevoRubro: RubroInsumo = {
+    id: Date.now(),
+    denominacion: nombreRubro,
+    activo: true,
+    rubroPadre: undefined,
+    subRubros: [],
+  };
+
+  // Si tiene rubro padre, se guarda como subrubro
+  if (tipoRubro === "Insumo" && rubroPadreSeleccionado) {
+    setRubros((prevRubros) =>
+      prevRubros.map((rubro) =>
+        rubro.denominacion === rubroPadreSeleccionado
+          ? {
+              ...rubro,
+              subRubros: [...rubro.subRubros, { ...nuevoRubro }]
+            }
+          : rubro
+      )
+    );
+  } else {
+    setRubros([...rubros, nuevoRubro]);
   }
 
+  abrirModal();
+  console.log("Nuevo rubro creado:", nuevoRubro);
+};
+
+  const agregarSubRubro = (rubroId: number, subRubroNombre: string) => {
+    setRubros(
+      rubros.map((rubro) =>
+        rubro.id === rubroId
+          ? { ...rubro, subRubros: [...rubro.subRubros, { id: Date.now(), denominacion: subRubroNombre, activo: true, subRubros: [] }] }
+          : rubro
+      )
+    );
+  };
+
+  const toggleSubCategorias = (rubroId: number) => {
+    setSubCategoriasAbiertas((prevState) => ({
+      ...prevState,
+      [rubroId]: !prevState[rubroId],
+    }));
+  };
+
+  const rubrosPadres = rubros.filter((r) => !r.rubroPadre);
 
   return (
     <>
@@ -34,45 +93,81 @@ const Categorias = () => {
         <button className='bg-secondary text-white px-3 py-2 rounded-2xl cursor-pointer' onClick={abrirModal}>+ Agregar Categoria</button>
       </div >
 
-      {/* ACA EMPIEZA LA TABLA  */}
-      < div className='mt-10 bg-white h-10 w-8/10 pl-10 flex items-center rounded-4xl text-base shadow-xl/10 font-semibold' > Nombre</div >
+      <div className="mt-10 bg-white h-10 w-8/10 pl-10 flex items-center rounded-4xl text-base shadow-xl/10 font-semibold">
+        Nombre
+      </div>
 
-
-
-
-      {/*Modal creacion de Categoria  */}
-      {
-        modalAbierto && (
-          <>
-            <div className="fixed inset-0 bg-black/50 z-40"></div>
-            <div className='rounded-3xl p-5 absolute top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 bg-primary  flex justify-center items-center shadow-lg w-5/10 flex-col'>
-              <button onClick={abrirModal} className='cursor-pointer absolute top-4 right-4 text-secondary hover:text-tertiary transition-colors'>
-                <FaTimes className='h-6 w-6' />
-              </button>
-              <h4 className='mt-5 font-bold text-2xl text-secondary'>Agregar nueva categoria</h4>
-              <form action="" className='flex flex-col mt-5 w-full gap-2'>
-                <label htmlFor="">Nombre</label>
-                <input type="text" name="" id="" placeholder='Nombre...' className='bg-white mb-2 py-2 p-0.5 pl-3 border rounded-2xl focus:border-none  focus:outline-3 focus:outline-[#BD1E22]' />
-                <label htmlFor="">Tipo:</label>
-                <div className='flex gap-10 items-center'>
-                  <label className='flex items-center gap-2' htmlFor="">Insumo <input type="radio" className='cursor-pointer' name="tipoCategoria" id="" /></label>
-
-                  <label className='flex items-center gap-2' htmlFor="">Producto <input type="radio" className='cursor-pointer' name="tipoCategoria" id="" /></label>
-
-                </div>
-                <div className='flex justify-around mt-5'>
-                  <button onClick={abrirModal} className='underline font-semibold cursor-pointer'>Cancelar</button>
-                  <button type='submit' className='py-1 px-4 font-semibold rounded-2xl text-white bg-tertiary cursor-pointer'>Crear</button>
-                </div>
-              </form>
+      {rubros.map((rubro) => (
+        <div key={rubro.id} className="w-8/10 mt-5 flex flex-col rounded-4xl text-base font-semibold">
+          <div className="flex items-center h-10">
+            <div className="w-1/2 pl-10 flex items-center gap-5">
+              <p className="underline">{rubro.denominacion}</p>
             </div>
-          </>
+            <div className="flex justify-end w-full pr-10 gap-5">
+              <FaPencilAlt className="text-black cursor-pointer transition-colors hover:text-tertiary" />
+              <FaRegTrashAlt className="text-black cursor-pointer transition-colors hover:text-tertiary" onClick={() => setRubros(rubros.filter((r) => r.id !== rubro.id))} />
+              <FaAngleUp onClick={() => toggleSubCategorias(rubro.id)} className={`text-black cursor-pointer transition-colors hover:text-tertiary h-5 transform ${subCategoriasAbiertas[rubro.id] ? "rotate-180" : ""}`} />
+            </div>
+          </div>
 
+          {subCategoriasAbiertas[rubro.id] && (
+            <div className="w-8/10 pl-16 pr-10 py-3">
+              <div className="mb-2">
+                {rubro.subRubros.map((subrubro) => (
+                  <p key={subrubro.id} className="text-sm">- {subrubro.denominacion}</p>
+                ))}
+              </div>
+            </div>
+          )}
 
-        )
-      }
+          <hr className="w-full"></hr>
+        </div>
+      ))}
+
+      {modalAbierto && (
+        <>
+          <div className="fixed inset-0 bg-black/50 z-40"></div>
+          <div className="rounded-3xl p-5 absolute top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 bg-primary flex justify-center items-center shadow-lg w-5/10 flex-col">
+            <button onClick={abrirModal} className="cursor-pointer absolute top-4 right-4 text-secondary hover:text-tertiary transition-colors"><FaTimes className="h-6 w-6" /></button>
+            <h4 className="mt-5 font-bold text-2xl text-secondary">Agregar nueva categoría</h4>
+            <form onSubmit={crearRubro} className="flex flex-col mt-5 w-full gap-2">
+              <label htmlFor="nombre">Nombre</label>
+              <input type="text" id="nombre" placeholder="Nombre..." value={nombreRubro} onChange={(e) => setNombreRubro(e.target.value)} className="bg-white mb-2 py-2 pl-3 border rounded-2xl focus:border-none focus:outline-3 focus:outline-[#BD1E22]" />
+
+              <label>Tipo:</label>
+              <div className="flex gap-10 items-center">
+                <label className="flex items-center gap-2">
+                  <input type="radio" className="cursor-pointer" name="tipoCategoria" value="Insumo" onChange={(e) => setTipoRubro(e.target.value)} />Insumo
+                </label>
+                <label className="flex items-center gap-2">
+                  <input type="radio" className="cursor-pointer" name="tipoCategoria" value="Producto" onChange={(e) => setTipoRubro(e.target.value)} /> Producto
+                </label>
+              </div>
+
+              {tipoRubro === "Insumo" && (
+                <>
+                  <label htmlFor="rubroPadre">Rubro Padre (Opcional)</label>
+                  <select id="rubroPadre" className="bg-white py-2 px-3 border rounded-2xl" onChange={(e) => setRubroPadreSeleccionado(e.target.value)}>
+                    <option value="">Seleccionar...</option>
+                    {rubrosPadres.map((rubro) => (
+                      <option key={rubro.id} value={rubro.denominacion}>
+                        {rubro.denominacion}
+                      </option>
+                    ))}
+                  </select>
+                </>
+              )}
+
+              <div className="flex justify-around mt-5">
+                <button type="button" onClick={abrirModal} className="underline font-semibold cursor-pointer">Cancelar</button>
+                <button type="submit" className="py-1 px-4 font-semibold rounded-2xl text-white bg-tertiary cursor-pointer">Crear</button>
+              </div>
+            </form>
+          </div>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Categorias
+export default Categorias;
