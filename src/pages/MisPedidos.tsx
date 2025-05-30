701080e158048b7b113518ccb4e12d1bcb7ef64e
flopigimenez <fgimenezmorcos@gmail.com>
2025-05-30 08:48:09 -0300

    mis pedidos-tabla

--- src/pages/MisPedidos.tsx
+++ src/pages/MisPedidos.tsx
@@ -1,6 +1,73 @@
+import { useState } from 'react';
 import { Header } from '../components/commons/Header'
+import { MisPedidosService } from '../services/MisPedidosService';
+import { useAppDispatch } from '../hooks/redux';
+import { PedidoResponseDTO } from '../types/Pedido/PedidoResponseDTO';
+
+// export interface PedidoResponseDTO {
+//     id?: number;
+//     fecha: string;           // LocalDate se convierte a string
+//     hora?: string;           // LocalTime se convierte a string
+//     codigo: string;
+//     estado: Estado;
+//     horaEstimadaFin?: string; // LocalTime se convierte a string
+//     tipoEnvio: TipoEnvio;
+//     totalVenta: number;      // Double se convierte a number
+//     totalCosto?: number;      // Double se convierte a number
+//     formaPago?: FormaPago;
+//     cliente?: ClienteResponseDTO;
+//     domicilio?: DomicilioResponseDTO;
+//     detallePedido?: DetallePedidoResponseDTO[];
+//     factura?: FacturaResponseDTO;
+// }
 
 const MisPedidos = () => {
+  const [loading, setLoading] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+
+  const misPedidosService = new MisPedidosService(); 
+  const dispatch = useAppDispatch();
+
+   const ColumnsTablePedido = [
+    {
+      label: "Orden",
+      key: "codigo",
+      // render: (pedido: PedidoResponseDTO) => ,
+    },
+    {
+      label: "Estado",
+      key: "estado",
+    },
+    {
+      label: "Envio",
+      key: "tipoEnvio",
+    },
+    {
+      label: "Fecha",
+      key: "fecha",
+    },
+    
+    {
+      label: "Total",
+      key: "total",
+    },
+    {
+      label: "Detalles",
+      key: "detalles",
+      render: (pedido: PedidoResponseDTO) => (
+        <button
+          className='bg-secondary text-white px-4 py-2 rounded'
+          onClick={() => {
+            // Aquí puedes abrir un modal o redirigir a una página de detalles
+            console.log(`Detalles del pedido ${pedido.codigo}`);
+          }}
+        >
+          Ver Detalles
+        </button>
+      ),
+    },
+   ];
+
   return (
     <div className='w-full h-screen bg-primary'>
         <Header/>
--- src/routes/RoutesApp.tsx
+++ src/routes/RoutesApp.tsx
@@ -8,6 +8,8 @@ import PedidosCocinero from '../pages/pedidosCocinero';
 import { ScreenInsumo } from '../pages/ScreenInsumo';
 import Configuracion from '../pages/admin/Configuracion';
 import { ScreenProducto } from '../pages/ScreenProducto';
+import MisPedidos from '../pages/MisPedidos';
+
 const RoutesApp = () => {
   return (
     <BrowserRouter>
@@ -16,6 +18,7 @@ const RoutesApp = () => {
         <Route path="/DetalleCompra" element={<DetalleCompra />} /> 
         {/* <Route path="/MisDirecciones" element={<MisDirecciones />} /> */}
         <Route path="/Menu" element={<Menu />} />
+        <Route path='/MisPedidos' element={<MisPedidos/>}/>
         <Route path="/PedidosCocinero" element={<PedidosCocinero />} />
         <Route path="/admin/Insumos" element={<ScreenInsumo />} />
         <Route path="/admin/productos" element={<ScreenProducto />} />
