0e536977bb483800a738b0239cccff7a65627c6a
Alvaro Rubina <alvarorubina132@gmail.com>
2025-05-30 21:40:06 -0300

    .

--- src/components/modals/ModalInsumo.tsx
+++ src/components/modals/ModalInsumo.tsx
@@ -94,7 +94,7 @@ export const ModalInsumo = ({
         stockMinimo: 0,
         esParaElaborar: false,
         activo: true,
-        unidadMedida: UnidadMedida.UNIDADES, // O el valor por defecto que corresponda
+        unidadMedida: "",
         rubroId: 0,
       };
 
