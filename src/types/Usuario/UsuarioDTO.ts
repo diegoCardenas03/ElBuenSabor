45e98ae147ef6f2c5d57e1e63364342676ca09e3
Alvaro Rubina <alvarorubina132@gmail.com>
2025-06-16 00:58:05 -0300

    actualizados types

--- src/types/Usuario/UsuarioDTO.ts
+++ src/types/Usuario/UsuarioDTO.ts
@@ -1,7 +1,8 @@
 export interface UsuarioDTO {
-    email: string;
-    contrasenia?: string;
-    connection?: string;
-    auth0Id?: string;
-    roles: string[];
+  email?: string
+  contrasenia?: string // Opcional para registro social
+  connection?: string
+  auth0Id?: string
+  roles: string[]
+  nickName?: string // Para registro social
 }
