e06c8becdd56055706209a858aa5ef3795904728
Diego Cardenas <diegoecduran2003@gmail.com>
2025-06-28 16:28:32 -0300

    arreglos auth0 modal

--- src/pages/CallBackPage.tsx
+++ src/pages/CallBackPage.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Loader } from "../components/commons/Loader";
 
 export const CallbackPage = () => {
-    const { isLoading, error, isAuthenticated } = useAuth0();
+    const { isLoading, error, isAuthenticated, logout } = useAuth0();
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -12,9 +12,22 @@ export const CallbackPage = () => {
             navigate("/login-redirect");
         }
         if (error) {
-            console.error("Auth0 error:", error);
+            logout();
+            // Mostrar mensaje y redirigir al home o login
+            setTimeout(() => {
+                navigate("/");
+            }, 2500);
         }
     }, [isLoading, isAuthenticated, error, navigate]);
+
+    if (error) {
+        return (
+            <div>
+                <Loader />
+                <h2>Error al iniciar sesion, redirigiendo...</h2>
+            </div>
+        );
+    }
 
     return (
         <div>
